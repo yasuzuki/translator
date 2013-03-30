4be3bd6422100ad3c6aa89726ddc08d657446bbe
Yasunori Suzuki <railsyas@gmail.com>
2013-03-30 23:53:55 +0900

    Created new rails application named translator.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Translator::Application.config.secret_token = '892fa19d8519f3eb1cafd820c1a7fd20a4c757ac98fc77951ffe96ff787e4f09c7935bbaee7fa3ebc584a41a58a9bbb9470cdfe3e777de60b73a83160dc0099c'
